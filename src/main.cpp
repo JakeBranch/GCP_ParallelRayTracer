e2ede50188bb78208b0f5c37dbb68ab8fc7ae8e4
= <jake.branch777@gmail.com>
2018-12-10 11:53:36 +0000

    fixed shadow merge

--- src/main.cpp
+++ src/main.cpp
@@ -121,19 +121,18 @@ void traceRays(int startY, int endY, int startX, int endX, std::shared_ptr<Camer
         
         for(int x = startX; x < endX; x++)
         {
-            
-            Ray ray = camera->createRay(glm::vec3(x, y, 0));
-
-            glm::vec3 color = glm::vec3(0.1f,0.1f,0.1f);
-
-            rayTracer->reset();
-            rayTracer->traceRay(ray, color);
-
-            color.x *= 255;
-            color.y *= 255;
-            color.z *= 255;
-            
             mutex.lock();
+                Ray ray = camera->createRay(glm::vec3(x, y, 0));
+
+                glm::vec3 color = glm::vec3(0.1f,0.1f,0.1f);
+
+                rayTracer->traceRay(ray, color);
+
+                color.x *= 255;
+                color.y *= 255;
+                color.z *= 255;
+            
+            // mutex.lock();
                 window->drawPixel(x, y, glm::clamp(color, glm::vec3(0,0,0), glm::vec3(255,255,255)));
             mutex.unlock();
         }
