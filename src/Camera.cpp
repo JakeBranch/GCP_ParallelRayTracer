371e275c666f786dc5b1aecf1b7a2d1b16e693ab
= <jake.branch777@gmail.com>
2018-12-12 09:27:28 +0000

    implemented camera projection

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,6 +1,8 @@
 #include "Camera.h"
 #include "Ray.h" //Remove??
 
+#include <glm/ext.hpp>
+
 Camera::Camera()
 {
     
@@ -8,10 +10,33 @@ Camera::Camera()
 
 Ray Camera::createRay(glm::vec3 pos)
 {
-    Ray ray;
+    glm::vec4 origin;
+    origin.x = (pos.x / (600.0f/2.0f)) - 1;
+    origin.y = -((pos.y / (800.0f/2.0f)) - 1);
+    origin.z = -1;
+    origin.w = 1;
 
-    ray.setOrigin(pos);
-    ray.setDirection(glm::vec3(0, 0, 1));
+    glm::vec4 destination;
+    destination.x = (pos.x / (600.0f/2.0f)) - 1;
+    destination.y = -((pos.y / (800.0f/2.0f)) - 1);
+    destination.z = 1;
+    destination.w = 1;
+
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 3.0f / 4.0f, 0.1f, 100.f); 
+    origin = glm::inverse(projection) * origin;
+    destination = glm::inverse(projection) * destination;
+
+    origin /= origin.w;
+    destination /= destination.w;
+
+    glm::mat4 view(1);
+    origin = glm::inverse(view) * origin;
+    destination = glm::inverse(view) * destination;
+
+    Ray ray;
+    ray.setOrigin(glm::vec3(origin));
+    glm::vec3 direction = glm::normalize(-glm::vec3(origin) + glm::vec3(destination));
+    ray.setDirection(direction);
 
     return ray;
 }
